293a44cc978629d8556a766ec88edc377ecc897d
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-08-27 19:37:08 -0400

    Initial user model

--- django_react_todo/settings.py
+++ django_react_todo/settings.py
@@ -39,10 +39,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    # 3rd party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.socialaccount',
+    'allauth.account',
 
     # Local apps
     'users',
 ]
+
+# django-allauth config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
