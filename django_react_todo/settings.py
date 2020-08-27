36e5f36f16f0e03635c3bf35606eba9bd13f741d
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-08-27 19:39:46 -0400

    Authenticate with email instead of username

--- django_react_todo/settings.py
+++ django_react_todo/settings.py
@@ -57,6 +57,15 @@ INSTALLED_APPS = [
 # django-allauth config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 SITE_ID = 1
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_UNIQUE_EMAIL = True
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
